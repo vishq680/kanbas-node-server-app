3f4da2cb235f41b65804555a45f203cfa4b875a5
Vishaq Jayakumar <vishaqj@Vishaqs-Laptop.local>
2023-12-05 15:09:29 -0500

    cors correction1

--- app.js
+++ app.js
@@ -18,14 +18,20 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 
 const cors = require('cors');
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
-app.use(
-    cors({
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    })
+// app.use(
+//     cors({
+//         credentials: true,
+//         origin: process.env.FRONTEND_URL
+//     })
 
-);
+// );
 
 const sessionOptions = {
     secret: "any string",
