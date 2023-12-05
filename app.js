16ae4f72bf5e8c077766230d883e90084c535453
Vishaq Jayakumar <vishaqj@Vishaqs-Laptop.local>
2023-12-05 01:47:47 -0500

    Minor changes

--- app.js
+++ app.js
@@ -5,13 +5,52 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+
+mongoose.connect(CONNECTION_STRING);
 
 
 
 const app = express()
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+// };
+
+app.use(
+    session(sessionOptions)
+);
+
 
 app.use(express.json());
+UserRoutes(app);
+
 ModuleRoutes(app);
 
 CourseRoutes(app);
